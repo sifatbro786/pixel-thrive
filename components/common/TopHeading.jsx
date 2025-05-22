38f358c9c88e480ba2b9840f42fbdc734362a083
sifatbro786 <solaimanislamsifat@gmail.com>
2025-05-22 13:40:57 +0600

    some styled fixed in milestone section

--- components/common/TopHeading.jsx
+++ components/common/TopHeading.jsx
@@ -3,7 +3,7 @@
 export default function TopHeading({heading, paragraph}) {
     return (
         <div className="flex flex-col md:flex-row md:items-center gap-4 md:gap-10">
-            <h2 className="w-fit px-1.5 bg-lime-300 rounded-md text-primary text-4xl font-medium">{heading}</h2>
+            <h2 className="w-fit px-1.5 bg-lime-300 rounded-md text-primary text-2xl md:text-4xl font-medium">{heading}</h2>
             <p className="w-auto text-justify md:w-[580px] text-lg font-normal leading-tight">{paragraph}</p>
         </div>
     );
--- components/milestone/Stats.jsx
+++ components/milestone/Stats.jsx
@@ -25,9 +25,9 @@ export default function Stats() {
     return (
         <div className="w-full mt-10 md:mt-[60px] flex flex-col md:flex-row items-center justify-between">
             {stats.map((stat) => (
-                <div key={crypto.randomUUID()} className="flex space-y-8 md:space-y-0">
+                <div key={crypto.randomUUID()} className="flex space-y-10 md:space-y-0">
                     <div>
-                        <p className="flex text-6xl font-normal uppercase tracking-widest justify-center">
+                        <p className="flex text-4xl md:text-6xl font-normal uppercase tracking-widest justify-center">
                             {stat?.number}
                             <span className="text-secondary">+</span>
                         </p>
